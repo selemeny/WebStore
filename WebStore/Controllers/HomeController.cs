3c290424f72ff5f51a3dee7398b4e7472bff1183
fuser85 <fuser85@myoffice365.site>
2020-03-10 00:31:32 +0300

    Пример работы с моделью

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        static readonly List<Employee> _employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                SurName = "Иванов",
+                FirstName = "Иван",
+                Patronymic = "Иванович",
+                Age = 39
+            },
+            new Employee
+            {
+                Id = 2,
+                SurName = "Петров",
+                FirstName = "Петр",
+                Patronymic = "Петрович",
+                Age = 18
+            },
+            new Employee
+            {
+                Id = 3,
+                SurName = "Сидоров",
+                FirstName = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 27
+            }
+        };
+
         public IActionResult Index()
         {
             return View();
@@ -17,6 +46,20 @@ namespace WebStore.Controllers
         public IActionResult SomeAction()
         {
             return View();
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
+        }
+
+        public IActionResult Employee(int Id)
+        {
+            var employee = _employees.FirstOrDefault(x => x.Id == Id);
+            if (employee is null)
+                return NotFound();
+
+            return View(employee);
         }
     }
 }
