1bb5565c34b4bfdecce8d5d250a595902da9d6e7
fuser85 <fuser85@myoffice365.site>
2020-03-09 23:04:21 +0300

    Пример работы с конфигурацией.

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -33,11 +33,13 @@ namespace WebStore
 
 			app.UseRouting();
 
+			var greetings = configuration["CustomGreetings"];
+
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapGet("/", async context =>
 				{
-					await context.Response.WriteAsync("Hello World!");
+					await context.Response.WriteAsync(greetings);
 				});
 			});
 		}
