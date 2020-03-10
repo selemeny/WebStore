84d97ff3579bc2c3a925d53af46dec8145485582
fuser85 <fuser85@myoffice365.site>
2020-03-10 18:45:32 +0300

    Сотрудники выделены в отдельный контроллер

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,35 +9,6 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        static readonly List<Employee> _employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                SurName = "Иванов",
-                FirstName = "Иван",
-                Patronymic = "Иванович",
-                Age = 39
-            },
-            new Employee
-            {
-                Id = 2,
-                SurName = "Петров",
-                FirstName = "Петр",
-                Patronymic = "Петрович",
-                Age = 18
-            },
-            new Employee
-            {
-                Id = 3,
-                SurName = "Сидоров",
-                FirstName = "Сидор",
-                Patronymic = "Сидорович",
-                Age = 27
-            }
-        };
-
-
         public IActionResult Index()
         {
             return View();
@@ -46,19 +17,6 @@ namespace WebStore.Controllers
 
 
         public IActionResult SomeAction() => View();
-
-
-        public IActionResult Employees() => View(_employees);
-
-
-        public IActionResult Employee(int Id)
-        {
-            var employee = _employees.FirstOrDefault(x => x.Id == Id);
-            if (employee is null)
-                return NotFound();
-
-            return View(employee);
-        }
 
 
         public IActionResult Error404() => View();
