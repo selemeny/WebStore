584d6bd191404b93561872f713fc9350cd6661b9
sel <sel@sel.ru>
2020-03-15 14:33:57 +0300

    Визуальный компонент секций

---
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    /// <summary>
+    /// Каталог товаров
+    /// </summary>
+    public interface IProductData
+    {
+        /// <summary>
+        /// Получить все секции
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Section> GetSections();
+        /// <summary>
+        /// Получить все бренды
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Brand> GetBrands();
+    }
+}
