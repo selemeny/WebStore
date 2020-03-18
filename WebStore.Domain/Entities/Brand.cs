c82c6f2439083ae88a0e5c5b3340e691f29aafcc
sel <sel@sel.ru>
2020-03-18 16:37:00 +0300

    Подготовка сущностей кразмещению в БД

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
@@ -9,8 +10,11 @@ namespace WebStore.Domain.Entities
     /// <summary>
     /// Бренд
     /// </summary>
+    //[Table("Brand")]
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
