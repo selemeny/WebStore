362daa3f7bc16b7314270465cc17d75bfb684aa1
sel <sel@sel.ru>
2020-03-18 17:28:07 +0300

    Контекст ДБ

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreDB : DbContext
+    {
+        public WebStoreDB(DbContextOptions<WebStoreDB> Options) : base(Options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
