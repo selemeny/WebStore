fa4aa9daf3a5f50d195aaa4f519dc5fd89275ca8
sel <sel@sel.ru>
2020-03-21 03:29:36 +0300

    Контроллер Account + представления к нему

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View();
+        public IActionResult Login() => View();
+    }
+}
