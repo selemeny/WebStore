59495227d2221c42a4003d80b3c39d55c17ab90f
sel <sel@sel.ru>
2020-03-21 12:35:06 +0300

    Модели представления механизма регистрации и авторизации

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register() => View();
-        public IActionResult Login() => View();
+        public IActionResult Register() => View(new RegisterUserViewModel());
+        public IActionResult Login() => View(new LoginViewModel());
     }
 }
