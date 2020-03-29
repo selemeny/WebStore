896904ed13f9dc988c14c37b9f3dd62cb5a996c4
sel <sel@sel.ru>
2020-03-29 21:54:25 +0300

    Интерфейс сервиса заказов

---
+++ WebStore/Infrastructure/Interfaces/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Orders;
+using WebStore.ViewModels;
+using WebStore.ViewModels.Orders;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    interface IOrderService
+    {
+        IEnumerable<Order> GetUserOrders(string Username);
+
+        Order GetOrderById(int id);
+
+        Order CreateOrder(string Username, CartViewModel cart, OrderViewModel orderModel);
+    }
+}
