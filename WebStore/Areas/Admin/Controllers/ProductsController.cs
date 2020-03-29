cc678ab06b8d86f466506ca782d1ab6ca653631f
sel <sel@sel.ru>
2020-03-29 20:52:58 +0300

    Список товаров в админке

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -5,12 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles =Role.Administrator)]
     public class ProductsController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly IProductData productData;
+
+        public ProductsController(IProductData productData) => this.productData = productData;
+
+
+        public IActionResult Index() => View(productData.GetProducts());
     }
 }
