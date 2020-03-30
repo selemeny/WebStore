cc3a28d2aa5c6c34eb35c81355fbd623765a9e76
sel <sel@sel.ru>
2020-03-30 22:09:48 +0300

    Заготовка профиля пользователя

---
+++ WebStore/Controllers/UserProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels.Orders;
+
+namespace WebStore.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Orders([FromServices] IOrderService orderService) => 
+            View(orderService.GetUserOrders(User.Identity.Name)
+                .Select(x => new UserOrderViewModel 
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Phone = x.Phone,
+                    Address = x.Address,
+                    TotalSum = x.OrderItems.Sum(i => i.Price * i.Quantity)
+                }));
+
+
+
+    }
+}
